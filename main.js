30fc268df6d0f0307aeff139b079f3cf3c35f0d6
Noxus1 <noxus1@gmail.com>
2024-03-24 10:14:58 -0400

    Logo update

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ $( document ).ready(function() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 8000); // Change image every 2 seconds
+    setTimeout(showSlides, 2000); // Change image every 8 seconds
   }
 });
 
